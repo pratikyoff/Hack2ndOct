4126b7d7227ac013e6e1c563bb955fa008a3fb29
Pratik Kumar <pratikyoff@gmail.com>
2020-10-03 20:48:33 +0530

    test level

--- Rube Goldberg Game/TestLevel/TestLevel.gd
+++ Rube Goldberg Game/TestLevel/TestLevel.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
+const magnet = preload("res://Magnet.tscn")
+const playerBall = preload("res://PlayerBallScene/PlayerBall.tscn")
+
+var rng = RandomNumberGenerator.new()
+
 func _ready():
-	var magnet = load("Magnet.tscn").instance()
-	magnet.position = Vector2(500,500)
-	add_child(magnet)
-	var playerBall = load("PlayerBallScene/PlayerBall.tscn").instance()
-	playerBall.position = Vector2(300,200)
-	add_child(playerBall)
+	rng.randomize()
+
+	var m = magnet.instance()
+	m.position = Vector2(500,500)
+	add_child(m)
+	
+	for _n in range(10):
+		var pb = playerBall.instance()
+		pb.position = Vector2(rng.randf_range(150, 1200), rng.randf_range(150, 250))
+		add_child(pb)
+	
